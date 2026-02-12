a28bc775b36544b04f9bcf2cad6f948fd6c76d0c
ZainabZain11 <zainabzainnn11@gmail.com>
2026-02-12 18:43:46 +0200

    task5,4.c sum all odd numbers among five inputs

---
+++ task5,4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main(){
+
+int num1=11;
+int num2=17;
+int num3=13;
+int num4=12;
+int num5=5;
+int sum=0;
+if(num1 %2 !=0 ){sum=sum+num1;}
+if(num2 %2 !=0){sum=sum+num2;}
+if(num3 %2 !=0){sum=sum+num3;}
+if(num4 %2 !=0){sum=sum+num4;}
+if(num5 %2 !=0){sum=sum+num5;}
+printf("sum =%d",sum);
+
+
+
+
+
+
+
+
+
+    return 0;
+}
