a1322070032b4168e592b7ecd08eb2bf3c49c481
ZainabZain11 <zainabzainnn11@gmail.com>
2026-02-12 18:40:44 +0200

    task5,2.c the distance between two point

---
+++ task5,2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<math.h>
+int main(){
+int x1=25;
+int y1=15;
+int x2=35;
+int y2=10;
+float distance;
+distance =sqrtf((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
+printf("distance =%f",distance);
+
+
+
+
+    return 0;
+}
