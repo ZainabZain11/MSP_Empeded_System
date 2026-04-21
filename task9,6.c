d5451b50d1bd7b7a96df3c4c7b2a234eb1e2e91a
ZainabZain11 <zainabzainnn11@gmail.com>
2026-04-21 15:08:56 +0200

    Multiply two matrices

---
+++ task9,6.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+int main(){
+int A[2][2]={{1,2},{3,4}};
+int B[2][2]={{4,6},{5,8}};
+int result[2][2];
+for(int i=0;i<2;i++){
+
+    for(int j=0;j<2;j++){
+        result[i][j]=0;
+
+        for(int k=0;k<2;k++){
+
+    result[i][j]+=A [i][k]* B [k][j]  ;      
+        }
+    }
+}
+printf("Result of Multiplication:\n");
+
+for(int i=0;i<2;i++){
+
+    for(int j=0;j<2;j++){
+printf("%d",result [i][j]);
+    }
+    printf("\n");
+}
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
